f6dfe10412fd413a7494cbf6ac06734f7df595d1
jordanmhart@gmail.com <jordan@jordanhart.com>
2017-09-03 22:00:15 -0700

    setup signin test

--- native/App.js
+++ native/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
+
+async function signInWithGoogleAsync() {
+  try {
+    const result = await Expo.Google.logInAsync({
+      androidClientId: '944363662100-vtptmm9aemekbkeijmv88i12n4n57dek.apps.googleusercontent.com',
+      androidStandaloneAppClientId: '944363662100-18mclfjmarm7ssnqo04mh1qpk72mqoi0.apps.googleusercontent.com',
+      scopes: ['profile', 'email'],
+    });
+
+    if (result.type === 'success') {
+      return result.user.email;
+    } else {
+      return 'cancelled';
+    }
+  } catch(e) {
+    console.log('Google Signin Error: ', e)
+    return 'error: ' + e;
+  }
+}
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      googleResponse: 'waiting for signin',
+    }
+    this.googleSigninCallback = this.googleSigninCallback.bind(this)
+  }
+
+  googleSigninCallback (googleResponse) {
+    console.log('googleResponse: ', googleResponse);
+    this.setState({
+      googleResponse: googleResponse,
+    })
+  }
+
+  componentDidMount () {
+    signInWithGoogleAsync()
+      .then(this.googleSigninCallback)
+      .catch(this.googleSigninCallback)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{this.state.googleResponse}</Text>
       </View>
     );
   }
